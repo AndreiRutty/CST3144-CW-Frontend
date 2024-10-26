01c1e80b2cc9f453f1737c02fa7ecc50234c0a2a
Andrei <andreirutty.ar20@outlook.com>
2024-10-26 08:44:28 +0400

    Implemented: Fetching courses data from server

--- script.js
+++ script.js
@@ -3,82 +3,25 @@ let app = new Vue({
   data: {
     displayCheckOut: false,
     searchFilter: "Subject",
-    courses: [
-      {
-        id: 1,
-        subject: "Math",
-        location: "Room 1",
-        price: 100,
-        spaces: 5,
-      },
-      {
-        id: 2,
-        subject: "English",
-        location: "Room 2",
-        price: 90,
-        spaces: 5,
-      },
-      {
-        id: 3,
-        subject: "Science",
-        location: "Room 3",
-        price: 110,
-        spaces: 5,
-      },
-      {
-        id: 4,
-        subject: "History",
-        location: "Room 4",
-        price: 95,
-        spaces: 5,
-      },
-      {
-        id: 5,
-        subject: "Geography",
-        location: "Room 5",
-        price: 85,
-        spaces: 5,
-      },
-      {
-        id: 6,
-        subject: "Art",
-        location: "Room 6",
-        price: 120,
-        spaces: 5,
-      },
-      {
-        id: 7,
-        subject: "Music",
-        location: "Room 7",
-        price: 130,
-        spaces: 5,
-      },
-      {
-        id: 8,
-        subject: "Physical Education",
-        location: "Gym",
-        price: 80,
-        spaces: 5,
-      },
-      {
-        id: 9,
-        subject: "Computer Science",
-        location: "Lab 1",
-        price: 140,
-        spaces: 3,
-      },
-      {
-        id: 10,
-        subject: "Biology",
-        location: "Room 8",
-        price: 115,
-        spaces: 2,
-      },
-    ],
+    courses: [],
     cart: [],
     search: "",
   },
   methods: {
+    fetchCourses() {
+      fetch("http://localhost:3000/courses")
+        .then((res) => {
+          if (!res.ok) throw new Error("Couldn't retrieve the courses");
+
+          return res.json();
+        })
+        .then((data) => {
+          this.courses = data;
+        })
+        .catch((err) => {
+          console.log(`Error: ${err}`);
+        });
+    },
     toggleCheckOut() {
       this.displayCheckOut = !this.displayCheckOut;
     },
@@ -139,5 +82,8 @@ let app = new Vue({
           course.location.toLowerCase().includes(this.search.toLowerCase())
       );
     },
+  },
+  mounted() {
+    this.fetchCourses();
   },
 });
