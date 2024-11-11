f5b7f9af6d69a40f19e42aaa9004a2990cb79f56
Andrei <andreirutty.ar20@outlook.com>
2024-11-11 09:40:42 +0400

    Implemented: Buying course functionality

--- script.js
+++ script.js
@@ -35,13 +35,13 @@ let app = new Vue({
     },
 
     findItemInCart(id) {
-      return this.cart.find((item) => item.id == id);
+      return this.cart.find((item) => item.courseId == id);
     },
 
     addToCart(item) {
       if (!this.findItemInCart(item.id) && item.spaces > 0) {
         this.cart.push(item);
-        item.spaces--;
+        // item.spaces--;
         alert(`${item.subject} course added to cart`);
       } else {
         alert("You already have added this course to your cart");
@@ -49,7 +49,7 @@ let app = new Vue({
     },
     removeItem(item) {
       this.cart = this.cart.filter((cartItem) => cartItem.id != item.id);
-      item.spaces++;
+      // item.spaces++;
     },
     sortCourses() {
       this.courses.sort((a, b) => {
@@ -69,12 +69,46 @@ let app = new Vue({
         }
       });
     },
+    placeOrder() {
+      const name = document.getElementById("name-input");
+      const phoneNumber = document.getElementById("phone-number-input");
+      const lessonIDs = [];
+
+      if (!this.cartItemCount) {
+        alert("No Items Bought");
+      } else {
+        this.coursesBought.forEach((course) => lessonIDs.push(course.courseId));
+
+        const order = {
+          name: name.value,
+          phoneNumber: phoneNumber.value,
+          lessonIDs: lessonIDs,
+          numberOfSpaces: 1,
+        };
+
+        fetch("http://localhost:3000/order", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(order),
+        })
+          .then((res) => res.json())
+          .catch((err) => console.log(err));
+
+        // Clearing Input Field
+        name.value = "";
+        phoneNumber.value = "";
+      }
+    },
   },
   computed: {
     cartItemCount() {
       return this.cart.length || "";
     },
-
+    coursesBought() {
+      return this.cart || [];
+    },
     filteredCourses() {
       return this.courses.filter(
         (course) =>
